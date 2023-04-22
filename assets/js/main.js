3ae47c56073ea3741c5906a9bf9e992e157e3c30
Abraham Castillo <abrahamcastillo090@gmail.com>
2023-04-21 21:12:51 -0600

    colocar otro html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,39 @@
 console.log("Hola desde un script externo");
 
-function changeColorTo(color){
-    const refName=document.getElementById("name");
-    refName.style.color="color";
+function changeColorTo(color) {
+    const refName = document.getElementById("name");
+    refName.style.color = color;
+}
+function ChangecolorParagraph(color, refObj) {
+    console.log(refObj)
+    refObj.style.color = color;
+}
+function resetColors() {
+    const refObjs = document.getElementsByClassName("text-color");
+    console.log(refObjs);
+    for (let i = 0; i < refObjs.length; i++) {
+        const element = refObjs[i];
+        element.style.color = "purple";
+
+    }
+}
+console.log("sesion web1");
+function changeGreeting() {
+    // obtener nombre cliente
+    const clientName = getnameOfClient();
+    // obenter referencia del label h1
+    const greetingReference = getReferenceOfIdGreeting();
+
+    // cambiar el saludo
+    greetingReference.innerHTML = "Hola " + clientName;
+}
+
+function getnameOfClient() {
+    const clientName = prompt("Escribe tu nombre");
+    return clientName;
+
+}
+
+function getReferenceOfIdGreeting() {
+    return document.getElementById("greeting");
 }
